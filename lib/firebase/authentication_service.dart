aa73f1a6fba40a9f418ab05f8ba211e5448697d6
prakarsh27 <prakarsh27@gmail.com>
2021-06-22 17:27:57 +0530

    Login/Signup Errors

--- lib/firebase/authentication_service.dart
+++ lib/firebase/authentication_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'user.dart';
 
@@ -19,7 +20,48 @@ class AuthenticationService {
           email: user.email, password: password);
       return 'Signed In';
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      switch (e.code) {
+        case "ERROR_EMAIL_ALREADY_IN_USE":
+        case "account-exists-with-different-credential":
+        case "email-already-in-use":
+          Fluttertoast.showToast(
+              msg: "Email already used. Go to login page.", fontSize: 20);
+          break;
+        case "ERROR_WRONG_PASSWORD":
+        case "wrong-password":
+          Fluttertoast.showToast(
+              msg: "Wrong email/password combination.", fontSize: 20);
+          break;
+        case "ERROR_USER_NOT_FOUND":
+        case "user-not-found":
+          Fluttertoast.showToast(
+              msg: "No user found with this email.", fontSize: 20);
+          break;
+        case "ERROR_USER_DISABLED":
+        case "user-disabled":
+          Fluttertoast.showToast(msg: "User disabled.");
+          break;
+        case "ERROR_TOO_MANY_REQUESTS":
+        case "operation-not-allowed":
+          Fluttertoast.showToast(
+              msg: "Too many requests to log into this account.", fontSize: 20);
+          break;
+        case "ERROR_OPERATION_NOT_ALLOWED":
+        case "operation-not-allowed":
+          Fluttertoast.showToast(
+              msg: "Server error, please try again later.", fontSize: 20);
+          break;
+        case "ERROR_INVALID_EMAIL":
+        case "invalid-email":
+          Fluttertoast.showToast(
+              msg: "Email address is invalid.", fontSize: 20);
+          break;
+        default:
+          Fluttertoast.showToast(
+              msg: "Login failed. Please try again.", fontSize: 20);
+          break;
+      }
+      return 'Login Failed';
     }
   }
 
@@ -39,7 +81,47 @@ class AuthenticationService {
       }
     } on FirebaseAuthException catch (e) {
       retVal = e.message;
-    } catch (e) {
+      switch (e.code) {
+        case "ERROR_EMAIL_ALREADY_IN_USE":
+        case "account-exists-with-different-credential":
+        case "email-already-in-use":
+          Fluttertoast.showToast(
+              msg: "Email already used. Go to login page.", fontSize: 20);
+          break;
+        case "ERROR_WRONG_PASSWORD":
+        case "wrong-password":
+          Fluttertoast.showToast(
+              msg: "Wrong email/password combination.", fontSize: 20);
+          break;
+        case "ERROR_USER_NOT_FOUND":
+        case "user-not-found":
+          Fluttertoast.showToast(
+              msg: "No user found with this email.", fontSize: 20);
+          break;
+        case "ERROR_USER_DISABLED":
+        case "user-disabled":
+          Fluttertoast.showToast(msg: "User disabled.", fontSize: 20);
+          break;
+        case "ERROR_TOO_MANY_REQUESTS":
+        case "operation-not-allowed":
+          Fluttertoast.showToast(
+              msg: "Too many requests to log into this account.", fontSize: 20);
+          break;
+        case "ERROR_OPERATION_NOT_ALLOWED":
+        case "operation-not-allowed":
+          Fluttertoast.showToast(
+              msg: "Server error, please try again later.", fontSize: 20);
+          break;
+        case "ERROR_INVALID_EMAIL":
+        case "invalid-email":
+          Fluttertoast.showToast(
+              msg: "Email address is invalid.", fontSize: 20);
+          break;
+        default:
+          Fluttertoast.showToast(
+              msg: "Login failed. Please try again.", fontSize: 20);
+          break;
+      }
       print(e);
     }
     return retVal;
